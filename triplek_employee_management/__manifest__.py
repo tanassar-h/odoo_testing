584fd291c832967f0c1d00afffd75ee5d3de5e89
tanassar-h <115463157+tanassar-h@users.noreply.github.com>
2023-01-19 16:09:15 +0500

    Update __manifest__.py

--- triplek_employee_management/__manifest__.py
+++ triplek_employee_management/__manifest__.py
@@ -7,7 +7,11 @@
     'version':'0.1',
     'author': 'Triplek Tech',
     'website': 'https://triplek.tech',
-    'description':'Triplek Tech Odoo Testing 1.0<b>version</b>',
+    'description':"""
+    Triplek Tech Odoo Testing 1.0
+    <b>version</b>
+    """
+    ,
     'category':'Human Resources',
     'license':'LGPL-3',
     'summary':'For Testing Purpose',
